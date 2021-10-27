46a9d334670a6c160b230e12d705adac50b7639f
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2021-10-27 15:05:27 +0700

    fixed error from merge

--- laravel/app/Providers/AppServiceProvider.php
+++ laravel/app/Providers/AppServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
-use Illuminate\pagination\paginator;
+
 
 
 class AppServiceProvider extends ServiceProvider
